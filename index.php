9b4d573593f7faa068913d50bd910e8ce9626e04
Joe <josephnewton861@yahoo.co.uk>
2020-09-15 18:45:11 +0100

    Error were shown directly on page  when nothing was visibly wrong therefore added a function to prevent these showing

--- index.php
+++ index.php
@@ -2,13 +2,6 @@
     $json = file_get_contents('news.json');
     $allData = json_decode($json, true);
     error_reporting(0);
-
-//    foreach($allData as $data) {
-//       $title = $data['attachments'][0]['title'];
-//         $image = $data['attachments'][0]['image_url'];
-//       $title_link = $data['attachments'][0]['title_link'];
-//    }
-
 ?>
 
 <!DOCTYPE html>
