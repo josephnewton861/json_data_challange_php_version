6a5410ce3516e80182fd6baae39c839d556caa2e
Joe <josephnewton861@yahoo.co.uk>
2020-09-15 17:23:41 +0100

    Added bootsrap for styling purposes

--- index.php
+++ index.php
@@ -2,12 +2,24 @@
     $json = file_get_contents('news.json');
     $allData = json_decode($json, true);
 
-   foreach($allData as $data) {
-      $title = $data['attachments'][0]['title'];
-        $image = $data['attachments'][0]['image_url'];
-      $title_link = $data['attachments'][0]['title_link'];
-       echo $title;
-       echo $image;
-   }
+//    foreach($allData as $data) {
+//       $title = $data['attachments'][0]['title'];
+//         $image = $data['attachments'][0]['image_url'];
+//       $title_link = $data['attachments'][0]['title_link'];
+//    }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>Kids_insights_challenge</title>
+</head>
+<body>
+
+    
+</body>
+</html>
