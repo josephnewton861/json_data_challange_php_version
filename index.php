d94d8b63a0cd777643f5881e680fb4527a6ad719
Joe <josephnewton861@yahoo.co.uk>
2020-09-15 17:26:56 +0100

    Added file for styling html elements

--- index.php
+++ index.php
@@ -16,10 +16,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css"href="index.css">
     <title>Kids_insights_challenge</title>
 </head>
 <body>
-
+<div class="container" >
+<h1>Kids Insights Challenge<h1>
+</div>
     
 </body>
 </html>
